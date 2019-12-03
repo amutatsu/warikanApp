c07764845e0041ceb7973feaf584f2c0b9b88a2c
amutatsu <amutatsu007@gmail.com>
2019-12-03 22:54:15 +0900

    Initial Commit

---
+++ warikanApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  warikanApp
+//
+//  Created by Tatsuya Amida on 2019/12/03.
+//  Copyright © 2019 Tatsuya Amida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
