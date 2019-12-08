e6e17e01fc68c7745339e4aac34ab4f602a5a0d5
amutatsu <amutatsu007@gmail.com>
2019-12-08 18:17:29 +0900

    [add]選択した税率ボタンの色が変化するよう機能を追加

--- warikanApp/ViewController.swift
+++ warikanApp/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var zeroButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var tenButton: UIButton!
+    @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var splitNumberLabel: UILabel!
         
     override func viewDidLoad() {
@@ -20,6 +24,22 @@ class ViewController: UIViewController {
     @IBAction func stepperPressed(_ sender: UIStepper) {
         splitNumberLabel.text = String(Int(sender.value))
     }
+    
+    @IBAction func taxButtonPressed(_ sender: UIButton) {
+        
+        billTextField.endEditing(true)
+        
+        zeroButton.isSelected = false
+        eightButton.isSelected = false
+        tenButton.isSelected = false
+        sender.isSelected = true
+        
+    }
+    
+    
+    
+    
+    
     
 }
 
