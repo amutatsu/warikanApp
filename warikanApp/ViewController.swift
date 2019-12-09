7d6333bfa2460f5eaf3bccb03602d162d975e1a4
amutatsu <amutatsu007@gmail.com>
2019-12-09 21:45:56 +0900

    [add]計算機能の追加

--- warikanApp/ViewController.swift
+++ warikanApp/ViewController.swift
@@ -15,7 +15,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var tenButton: UIButton!
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var splitNumberLabel: UILabel!
-        
+    
+    var taxPersentage = 0.0
+    var totalBill = 0.0
+    var numberOfPeople = 0
+    var result = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -34,12 +39,18 @@ class ViewController: UIViewController {
         tenButton.isSelected = false
         sender.isSelected = true
         
+        let taxButtonTitleWithoutPersent = sender.currentTitle!.dropLast()
+        taxPersentage = Double(taxButtonTitleWithoutPersent)!
+        
     }
     
-    
-    
-    
-    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        if let bill = billTextField.text {
+            totalBill = Double(bill)!
+            result = totalBill * (1 + taxPersentage/100)
+        }
+    }
     
 }
 
