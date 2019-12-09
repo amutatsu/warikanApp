331f2ddadf01821e0085564e35d53df34e80984d
amutatsu <amutatsu007@gmail.com>
2019-12-09 22:45:33 +0900

    [add]計算結果画面の追加

---
+++ warikanApp/ResultViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ResultViewController.swift
+//  warikanApp
+//
+//  Created by Tatsuya Amida on 2019/12/09.
+//  Copyright © 2019 Tatsuya Amida. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- warikanApp/ViewController.swift
+++ warikanApp/ViewController.swift
@@ -52,5 +52,9 @@ class ViewController: UIViewController {
         }
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+     }
 }
 
